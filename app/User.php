3cf1d6f5fc617988d625c93b9a27b32f9ddc6268
Andreew Januario <andreew.k.januario@outlook.com>
2022-01-19 19:48:51 -0300

    insert new column on table user and create model Markup

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','post',
     ];
 
     /**
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function markup()
+    {
+        //Informando que uma temporada possui varios episódios
+        return $this->hasMany(markup::class);
+    }
 }
